77253f37a8a2b52aaf2f4969d1de3f99b41bfdfd
Kunto Mustiko <kuntomustiko@gmail.com>
2019-09-24 11:03:15 +0700

    action login

--- app/src/main/java/com/minara/kirana/manjuaapp/login/LoginActivity.kt
+++ app/src/main/java/com/minara/kirana/manjuaapp/login/LoginActivity.kt
@@ -1,7 +1,13 @@
 package com.minara.kirana.manjuaapp.login
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
 import com.minara.kirana.manjuaapp.MainActivity
 import com.minara.kirana.manjuaapp.R
 import com.minara.kirana.manjuaapp.login.data.User
@@ -15,12 +21,22 @@ import org.jetbrains.anko.startActivity
 import org.jetbrains.anko.yesButton
 
 class LoginActivity : AppCompatActivity(), LoginView {
+
+
+    private var googleSignInClient : GoogleSignInClient? = null
+
     //2
     private lateinit var presenter: LoginPresenter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        // Configure Google Sign In
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestEmail()
+            .build()
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
 
         //3
         presenter = LoginPresenter(this)
@@ -37,6 +53,37 @@ class LoginActivity : AppCompatActivity(), LoginView {
         linkRegister.onClick {
             startActivity<RegisterActivity>()
         }
+
+        btnLoginGoogle.onClick {
+            signIn()
+        }
+    }
+
+    private fun signIn() {
+        val signInIntent = googleSignInClient?.signInIntent
+        startActivityForResult(signInIntent, 1)
+    }
+
+    public override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == 1) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                val account = task.getResult(ApiException::class.java)
+                            Log.d("signin email", account?.email.toString())
+
+            } catch (e: ApiException) {
+                // Google Sign In failed, update UI appropriately
+//                Log.w(TAG, "Google sign in failed", e)
+                Log.d("sign in email", e.statusCode.toString())
+                // ...
+
+            }
+        }
     }
 
     //4
@@ -52,4 +99,11 @@ class LoginActivity : AppCompatActivity(), LoginView {
         }.show()
     }
 
+    //hari k2
+    override fun onSuccessLoginGmail(user: User?, msg: String?) {
+        if (msg == "login gmail"){
+            startActivity<MainActivity>()
+            finish()
+        }
+    }
 }
